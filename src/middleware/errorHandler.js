cbf3a1bd540205d3f44aa5e2224b8f9448d86a11
great-park <cksgh1735@gmail.com>
2023-04-19 21:01:40 +0900

    feat: error handler

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,16 @@
+const winston = require("../../config/winston");
+
+const errorHandler = (err, req, res, next) => {
+  const status = err.status !== undefined ? err.status : 500;
+  if (status === 500) {
+    winston.error(err.stack || '');
+  }
+  res.status(status).json({
+    error: {
+      message: err.message,
+      code: err.code,
+    },
+  });
+};
+
+module.exports = errorHandler;
