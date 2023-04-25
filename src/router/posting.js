3d5787f2584fc2fef34ad8d71d292d69419399e2
great-park <cksgh1735@gmail.com>
2023-04-25 15:51:34 +0900

    feat: add router

--- src/router/posting.js
+++ src/router/posting.js
@@ -6,5 +6,6 @@ const ctrl = require('../controller/posting.ctrl');
 router.post('/', ctrl.publish);
 router.get('/', ctrl.fetchList);
 router.get('/:id', ctrl.fetchOne);
+router.post('/comment', ctrl.addComment);
 
 module.exports = router;
