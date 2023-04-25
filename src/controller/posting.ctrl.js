1316bb992a20872d2045a43f56ded01adc032e11
great-park <cksgh1735@gmail.com>
2023-04-25 15:42:44 +0900

    feat: add comment

--- src/controller/posting.ctrl.js
+++ src/controller/posting.ctrl.js
@@ -34,7 +34,28 @@ const postingController = {
         .then((postings) => {
             res.status(200).json(postings);
         });
-    }
+    },
+
+    addComment: (req, res) => {
+        const { id } = req.params;
+      
+        const newComment = new Comment({
+          name: req.body.name,
+          content: req.body.content,
+          posting_id: id
+        });
+      
+        newComment.save()
+          .then(comment => {
+            Posting.findByIdAndUpdate(id, { $push: { comments: comment._id } })
+              .then(() => {
+                res.status(200).json({ message: 'Comment added successfully' });
+              });
+          })
+          .catch(err => {
+            res.status(500).json({ error: err.message });
+          });
+      }
 }
 
 module.exports = postingController;
